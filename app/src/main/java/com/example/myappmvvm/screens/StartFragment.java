964c4750dfddab128a624bc5d6eb7b54d9d86b52
Koviello Arlekkino <vainglory.ru@mail.ru>
2022-12-19 13:36:55 +0300

    ADD: class AppDatabase

---
+++ app/src/main/java/com/example/myappmvvm/screens/StartFragment.java
@@ -0,0 +1,20 @@
+package com.example.myappmvvm.screens;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myappmvvm.R;
+
+public class StartFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+}
