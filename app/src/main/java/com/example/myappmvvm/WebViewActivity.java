57ae4ca95c4e9a507d8478703ad8be259e9f204c
Koviello Arlekkino <vainglory.ru@mail.ru>
2023-01-20 19:18:57 +0300

    ADD: get access_token

--- app/src/main/java/com/example/myappmvvm/WebViewActivity.java
+++ app/src/main/java/com/example/myappmvvm/WebViewActivity.java
@@ -1,8 +1,18 @@
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myappmvvm.MainActivity;
 
 public class WebViewActivity<ActivityWebViewBinding> extends AppCompatActivity {
 
@@ -27,4 +37,29 @@ public class WebViewActivity<ActivityWebViewBinding> extends AppCompatActivity {
         System.out.println(mBinding.webView.getUrl());
     }
 
+    public WebViewClient oauth2Vk(WebViewActivity activity){
+        return new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                if(request.getUrl().toString().contains("https://oauth.vk.com/blank.html#")){
+                    Log.e(TAG, "Response " + request.getUrl().toString());
+                    String token = Uri.parse(request.getUrl().toString().replace("#", "?")).getQueryParameter("access_token");
+                    String email = Uri.parse(request.getUrl().toString().replace("#", "?")).getQueryParameter("email");
+                    Log.e(TAG, "Access token " + email);
+                    DataModel user = ServiceLocator.getInstance().getCurrentUser();
+                    user.setId(token);
+                    user.setEmail(email);
+                    if(token != null){
+                        Intent intent = new Intent(activity, MainActivity.class);
+                        intent.putExtra("token", token);
+
+                        activity.startActivity(intent);
+                    }
+                    return false;
+                }
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        };
+    }
 }
